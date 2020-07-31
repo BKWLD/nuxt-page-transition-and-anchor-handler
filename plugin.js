ae44b2cd6fb5886d4b05a9747829b8a8e26690c3
Robert Reinhard <robert.reinhard@bukwild.com>
2020-07-31 16:47:00 -0700

    Whitespace

--- plugin.js
+++ plugin.js
@@ -6,7 +6,7 @@ const options = JSON.parse('<%= options %>');
 
 // Export the plugin config
 export default function({ app, route, store }, inject) {
-	
+
 	// Add our VueX module to the store.  I tried to make a seperate file but
 	// something gets confused in the Nuxt module
 	store.registerModule('ptah', {
@@ -17,53 +17,53 @@ export default function({ app, route, store }, inject) {
 			isTransitioning: false,
 		},
 		mutations: {
-			
+
 			// Track scrolling
 			startScroll: function(state, promise) {
 				state.scrolling = promise;
 				state.isScrolling = true
 			},
-			
+
 			// Mark the scroll stopped
 			stopScroll: function(state) {
 				state.isScrolling = false
 			},
-			
+
 			// Track transitioning
 			transitioning: function(state, bool) {
 				state.isTransitioning = bool
 			},
 		}
 	})
-	
+
 	// Scroll and store whether a scroll is happening in vuex
 	const scrollTo = function(target) {
 		store.commit('ptah/startScroll', animatedScrollTo(target, {
 			...options.animatedScrollTo,
 		})
-			
+
 		// Update the scolling boolean after it's done
 		.then(function() {
 			store.commit('ptah/stopScroll')
 		}))
 	}
-	
+
 	// Scroll to the current anchor on the page
 	const scollToHash = function() {
-		
+
 		// Require a hash
 		if (!app.router.currentRoute.hash) return
 		const anchor = app.router.currentRoute.hash.substring(1)
-		
+
 		// Check for element on the page to match the hash
 		const selector = options.anchorSelector.replace('{{anchor}}', anchor)
 		const el = document.querySelector(selector)
 		if (!el) return
-		
+
 		// Scroll to the anchor
 		scrollTo(el)
 	}
-	
+
 	// React to route changes when only the hash changes.  Other page changes are
 	// handled by the global layout transition.
 	app.router.afterEach(function (to, from) {
@@ -71,31 +71,30 @@ export default function({ app, route, store }, inject) {
 			scollToHash();
 		}
 	})
-	
+
 	// Listen for the initial page build and then wait a bit for it to finish
 	// rendering.
 	window.onNuxtReady(function() {
 		return setTimeout(scollToHash, options.initialDelay);
 	})
-	
-	// Inject a scroll to top helper 
+
+	// Inject a scroll to top helper
 	inject('scrollTo', scrollTo)
-	
-	// Inject a scroll to top helper 
+
+	// Inject a scroll to top helper
 	inject('scrollToTop', function () {
 		scrollTo(0)
 	})
-	
+
 	// Syntactic sugare for getitng the scrolling boolean
 	inject('scrollComplete', function () {
 		return store.state.ptah.scrolling;
 	})
-	
+
 	// Handle beforeLeave transition events (e.g. the transition has started)
 	inject('beforePageLeave', function() {
 		this.$store.commit('ptah/transitioning', true)
 	})
-	
 	// Handle afterEnter transition events (e.g. the transition is done)
 	inject('afterPageEnter', function() {
 		this.$store.commit('ptah/transitioning', false)
